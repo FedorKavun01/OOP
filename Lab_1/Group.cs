c4dc72c5af8b004abf5afdafb2fdf2bb3fea3ab0
Unknown <omax1972@gmail.com>
2019-09-16 18:28:16 +0300

    Create Student and Group classes

---
+++ Lab_1/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Group
+    {
+        public String name;
+        public List<Student> students; 
+    }
+}
---
+++ Lab_1/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Student
+    {
+        public string firstName;
+        public string lastName;
+        public string fullName;
+        public Group group;
+        private uint[] marks = new uint[3];
+
+        public Student(string firstName,string lastName, Group group, uint[] marks)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.fullName = firstName + " " + lastName;
+            this.group = group;
+            group.students.Add(this);
+            this.marks = marks;
+        }
+    }
+}
